18ccf460578ab32edc4781fc2e351b0df45c43a5
Chakrapani Kulkarni <chakrapani.kulkarni@gmail.com>
2020-08-05 19:31:12 +0530

    adding rupee symbol

---
+++ docs/precache-manifest.e4c44d6584bf9ced25647d803eba2763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02358829e88cf05db13bbfff1dc6cb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8380974888dfa7c0d071",
+    "url": "/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8380974888dfa7c0d071",
+    "url": "/static/js/main.bc69c4a0.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,15 +1,22 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 export const Transaction = ({ transaction }) => {
-    const sign = transaction.amount < 0 ? '-' : '+';
-    const { deleteTransaction } = useContext(GlobalContext);
-    return (
-        <div>
-            <li className={transaction.amount > 0 ? "plus" : "minus"}>
-                {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-                <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
-            </li>
-        </div>
-    )
-}
-
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const { deleteTransaction } = useContext(GlobalContext);
+  return (
+    <div>
+      <li className={transaction.amount > 0 ? "plus" : "minus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}&#8377;{Math.abs(transaction.amount)}
+        </span>
+        <button
+          onClick={() => deleteTransaction(transaction.id)}
+          className="delete-btn"
+        >
+          x
+        </button>
+      </li>
+    </div>
+  );
+};
